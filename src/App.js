1184ad41a3197214f716747a48ed1a15c7c5fd35
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-01 18:33:06 -0400

    Added CRUD operations with repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
+function buildRepository(baseNumber) {
+  return {
+    title: `Repository ${baseNumber}`,
+    url: `https://github.com/natanaelsirqueira/repository-${baseNumber}`,
+    techs: []
+  }
+}
+
 function App() {
+  const [repositories, setRepositories] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(1)
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', buildRepository(currentIndex))
+
+    setRepositories([...repositories, response.data])
+    setCurrentIndex(currentIndex + 1)
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    setRepositories(repositories.filter(repository => repository.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
